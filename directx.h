4675762f72734b8efce276717c34aef7164a5e4c
James <serratedx@gmail.com>
2012-01-28 15:15:40 -0800

    v 0.112
    Holds new sprite info!

--- directx.h
+++ directx.h
@@ -1,20 +1,27 @@
-#include <windows.h>
+#pragma once
+
 #include <d3d9.h>
+#include <d3dx9tex.h>
+#include <string>
+
+#define SCRN_WIDTH 640			// the width of the screen
+#define SCRN_HEIGHT 480			// the height of the screen
 
 class DXInterface
 {
 private:
-	HINSTANCE				hInstance;
-	HWND					wndHandle;
+	HRESULT                 lastResult;
 	LPDIRECT3D9             pD3D;
 	LPDIRECT3DDEVICE9       pd3dDevice;
-
 public:
-	DXInterface(){};
-	virtual ~DXInterface(){};
-	virtual bool initWindow(HINSTANCE hInstance);
-	virtual bool initDirect3D();
-	virtual void render();
-	virtual void cleanup();
+	DXInterface(void);
+	~DXInterface(void);
+	bool init(HWND hwnd);
+	void shutdown(void);
+	void beginRender(void);
+	void endRender(void);
+	IDirect3DSurface9* getSurfaceFromBitmap(std::string filename);
 
+	IDirect3DSurface9* getBackBuffer(void);
+	void blitToSurface(IDirect3DSurface9* srcSurface, const RECT *srcRect, const RECT *destRect);
 };
